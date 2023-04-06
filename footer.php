52370c51d2434850568f2bbede4c6550f65f9a65
Ivo Binggeli <binggelivo@bluewin.ch>
2023-04-06 14:10:27 +0200

    init

---
+++ footer.php
@@ -0,0 +1,26 @@
+    <div class="container footer">
+        <div class="block block100 beige rounded">
+            <h2>Umgesetzt durch unsere Vereine</h2>
+            <div class="footer-logos">
+                <div class="footer-logo"><img src="img/schutzen.webp" alt="Logo Schützen Bettlach"></div>
+                <div class="footer-logo"><img src="img/turnverein.webp" alt="Logo Turnverein Bettlach"></div>
+                <div class="footer-logo"><img src="img/ogv.webp" alt="Logo Obst und Gartenbau Verein Bettlach"></div>
+                <div class="footer-logo"><img src="img/zs.webp" alt="Logo Zänteschür Verein"></div>
+                <div class="footer-logo"><img src="img/shc.webp" alt="Logo Street Hockey Club Bettlach"></div>
+                <div class="footer-logo"><img src="img/spielgruppe.webp" alt="Logo Spielgruppenverein Bettlach"></div>
+                <div class="footer-logo"><img src="img/comitato.webp" alt="Logo Comitato Bettlach"></div>
+                <div class="footer-logo"><img src="img/pfadi.webp" alt="Logo Pfadi Bettlach"></div>
+                <div class="footer-logo"><img src="img/boogianer.webp" alt="Logo Böögianer Bettlach"></div>
+                <div class="footer-logo"><img src="img/alphorn.webp" alt="Logo Alphorn Bettlach"></div>
+            </div>
+            <h2>Unterstützt durch</h2>
+            <div class="unterstutzer-logos">
+                <div class="unterstutzer-logo"><img src="img/gewerbverein.webp" alt="Logo Gewerbeverein Bettlach"></div>
+                <div class="unterstutzer-logo"><img src="img/jkskb.webp" alt="Logo Jugend-, Kultur- und Sportkomission Bettlach"></div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <title>Home - Bettle-Trail</title>
+    <link rel="stylesheet" href="https://use.typekit.net/eiv3lql.css">
+    <link rel="stylesheet" href="css.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=" Dieser Rätselweg der Bettlacher Vereine führt durchs Dorf und eignet sich für Jung und Alt. Der Rundgang dauert 1.5-2h (reine Gehzeit ca. 40min.).">
+</head>
+
+<body>
+    <div class="header container centered margin-b5">
+        <img src="img/logo.webp" alt="Logo Bettle-Trail" class="logo">
+    </div>
