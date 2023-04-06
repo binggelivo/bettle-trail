157ce9296457476b012ff00be7a5b2a2d702e38b
Ivo Binggeli <binggelivo@bluewin.ch>
2023-04-07 01:21:07 +0200

    debug points

--- backend/points.php
+++ backend/points.php
@@ -3,7 +3,7 @@ echo $_POST["points"];
 echo "<br>";
 echo htmlspecialchars($_SESSION["points"]);
 if ($_POST["points"]){
-    $_SESSION["points"] = $_SESSION["points"] + $_POST["points"];
+    $_SESSION["points"] = htmlspecialchars($_SESSION["points"]) + $_POST["points"];
 }
 echo "<br>";
 echo htmlspecialchars($_SESSION["points"]);
