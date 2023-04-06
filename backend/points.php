349d1164ff5bc53c84933419f4c10bb146c10ef3
Ivo Binggeli <binggelivo@bluewin.ch>
2023-04-07 01:28:24 +0200

    debug

--- backend/points.php
+++ backend/points.php
@@ -3,7 +3,7 @@ echo "neue: " . $_POST["points"];
 echo "<br>";
 echo "vorher: " . htmlspecialchars($_SESSION["points"]);
 if ($_POST["points"]){
-    $_SESSION["points"] = htmlspecialchars($_SESSION["points"]) + $_POST["points"];
+    $_SESSION["points"] += $_POST["points"];
 }
 echo "<br>";
 echo "jetzt: " . htmlspecialchars($_SESSION["points"]);
