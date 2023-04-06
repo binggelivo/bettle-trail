486084b34d6c22572501718df76233789c999178
Ivo Binggeli <binggelivo@bluewin.ch>
2023-04-06 16:45:21 +0200

    icos

--- header.php
+++ header.php
@@ -6,6 +6,13 @@
     <title>Home - Bettle-Trail</title>
     <link rel="stylesheet" href="https://use.typekit.net/eiv3lql.css">
     <link rel="stylesheet" href="css.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content=" Dieser Rätselweg der Bettlacher Vereine führt durchs Dorf und eignet sich für Jung und Alt. Der Rundgang dauert 1.5-2h (reine Gehzeit ca. 40min.).">
 </head>
