230fd8fa8270be70d3432665509e7754c0bcd5c7
Ivo Binggeli <binggelivo@bluewin.ch>
2023-04-06 20:08:30 +0200

    test

--- backend/bilder.php
+++ backend/bilder.php
@@ -4,17 +4,20 @@ session_start();
 $servername = "localhost";
 $username = "binggeli_bettle-tail";
 $password = "eQQp2Ats8fPcdzJqCXj8";
+$dbname = "binggeli_bettle-tail";
 
 
 
 if (isset($_POST["art"])){
     if ($_POST["art"] == "onboarding"){
         print_r(basename($_FILES["photo"]["name"]));
+        echo "<br><br>";
         print_r(strtolower(pathinfo($target_file,PATHINFO_EXTENSION)));
+        echo "<br><br>";
         /*if (move_uploaded_file($_FILES["photo"]["tmp_name"], "uploads/" . microtime())){
 
         }*/
-        $conn = new mysqli($servername, $username, $password);
+        $conn = new mysqli($servername, $username, $password, $dbname);
         $sql = "INSERT INTO user (user_startpic, user_name, user_private)
         VALUES (" . "," . $_POST["groupname"] . ", " . $_POST["privat"] . ");";
         
