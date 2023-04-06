a886827c88158c2c47a83881e97fdc2659f96c3e
Ivo Binggeli <binggelivo@bluewin.ch>
2023-04-06 20:41:43 +0200

    fasdasd

--- backend/bilder.php
+++ backend/bilder.php
@@ -10,17 +10,21 @@ $dbname = "binggeli_bettle-trail";
 
 if (isset($_POST["art"])){
     if ($_POST["art"] == "onboarding"){
-        print_r(basename($_POST["photo"]["name"]));
+        print_r(basename($_FILES["photo"]["name"]));
         echo "<br><br>";
-        print_r(strtolower(pathinfo(basename($_POST["photo"]["name"]),PATHINFO_EXTENSION)));
+        print_r(strtolower(pathinfo(basename($_FILES["photo"]["name"]),PATHINFO_EXTENSION)));
         echo "<br><br>";
         $time = time();
         echo "<br><br>";
         print_r($time);
         echo "<br><br>"; 
-        print_r(basename($time . "." . strtolower(pathinfo(basename($_POST["photo"]["name"]),PATHINFO_EXTENSION))));
+        print_r(basename($time . "." . strtolower(pathinfo(basename($_FILES["photo"]["name"]),PATHINFO_EXTENSION))));
         echo "<br><br>";
-        move_uploaded_file($_POST["photo"]["tmp_name"], "uploads/" . basename($time . "." . strtolower(pathinfo(basename($_POST["photo"]["name"]),PATHINFO_EXTENSION))));
+        if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["photo"]["name"])). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
         echo "<br><br>";
         $conn = new mysqli($servername, $username, $password, $dbname);
         $sql = "INSERT INTO user (user_startpic, user_name, user_private)
